7ef9568bb8b297f502c3652fb2d7fa1b25cebb54
Yosuke Horie <Yosuke.Horie@sony.com>
2024-06-23 13:57:42 +0900

    Add dropdown to select target variable

--- lightgbm-app.py
+++ lightgbm-app.py
@@ -7,16 +7,18 @@ from sklearn.model_selection import train_test_split
 
 def main():
     st.title('LightGBM App')
-    st.write('Welcome to the LightGBM App!')
 
     # load dataset from file dialog
     uploaded_file = st.file_uploader('Upload a dataset', type=['csv', 'txt'])
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file, header=0)
-        st.write(df)
+        # st.write(df)
 
-        X = df.drop('MEDV', axis=1)
-        y = df['MEDV']
+        # dropdown list for target variable
+        target_variable = st.selectbox('Select the target variable', df.columns)
+
+        X = df.drop(target_variable, axis=1)
+        y = df[target_variable]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
         lgb_train = lgb.Dataset(X_train, y_train)
 
