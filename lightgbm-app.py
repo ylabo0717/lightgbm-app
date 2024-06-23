1905e19b284baf88272aa701ee83124311881b93
Yosuke Horie <Yosuke.Horie@sony.com>
2024-06-23 15:07:01 +0900

    Add side-bar

--- lightgbm-app.py
+++ lightgbm-app.py
@@ -3,11 +3,17 @@ import numpy as np
 import lightgbm as lgb
 import shap
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import streamlit as st
 from streamlit_shap import st_shap
 
 def main():
     st.title('LightGBM App')
+
+    # LightGBM parameters from User settings
+    st.sidebar.title('LightGBM Parameters')
+    num_leaves = st.sidebar.slider('num_leaves', 2, 50, 5)
+    num_boost_round = st.sidebar.slider('num_boost_round', 10, 100, 50)
 
     # load dataset from file dialog
     uploaded_file = st.file_uploader('Upload a dataset', type=['csv'])
@@ -22,15 +28,20 @@ def main():
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
         lgb_train = lgb.Dataset(x_train, y_train)
 
+        # LightGBM parameters
         params = {
             'objective': 'mse',
-            'num_leaves': 5,
+            'num_leaves': num_leaves,
             'seed': 0,
             'verbose': -1
         }
 
         # train LightGBM model
-        model = lgb.train(params, lgb_train, num_boost_round=50, valid_sets=[lgb_train], valid_names=['train'], callbacks=[lgb.log_evaluation(10)])
+        model = lgb.train(params, lgb_train, num_boost_round=num_boost_round, valid_sets=[lgb_train], valid_names=['train'], callbacks=[lgb.log_evaluation(10)])
+
+        # test
+        y_pred = model.predict(x_test, num_iteration=model.best_iteration)
+        st.write('RSME', mean_squared_error(y_test, y_pred) ** 0.5)
 
         st.write('SHAP summary plot')
         explainer = shap.TreeExplainer(model=model, feature_perturbation='tree_path_dependent')
